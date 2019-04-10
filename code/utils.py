4d7cd361465b57bd33ce2502cd1d7b31c73af9d5
pcortajarena <pcortajarena@gmail.com>
2019-04-10 15:40:02 +0000

    code: arrange main and utils and first gan implementation

---
+++ code/utils.py
@@ -0,0 +1,26 @@
+import os
+from skimage import color, filters, img_as_float, img_as_ubyte
+from skimage.feature import blob_dog
+import matplotlib.pyplot as plt
+
+def read_images_path(path, images_list):
+    for x in os.listdir(path):
+        path2 = os.path.join(path, x)
+        if os.path.isdir(path2):
+            read_images_path(path2, images_list)
+        elif os.path.isfile(path2) and path2.endswith('.jpg'):
+            images.append(path2)
+
+def transform_dog(image_path, sigma):
+    img = plt.imread(image_path)
+    img = img_as_float(img)
+    img_gray = color.rgb2gray(img)
+    
+    k = 1.8
+    s1 = filters.gaussian(img_gray ,k*sigma)
+    s2 = filters.gaussian(img_gray ,sigma)
+    dog = (s1 - s2)
+    dog = 255 - img_as_ubyte(dog)
+    
+    path2 = image_path + '_dog.jpg'
+    plt.imsave(path2, dog, cmap='gray')
